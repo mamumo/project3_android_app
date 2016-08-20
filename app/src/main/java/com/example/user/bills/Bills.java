a7bbbc227f1a3058fd93a4ad57f279885b4724fb
Marie Moyles <marie.moyles2@mail.dcu.ie>
2016-08-20 17:57:04 +0100

    initial commit

---
+++ app/src/main/java/com/example/user/bills/Bills.java
@@ -0,0 +1,17 @@
+package com.example.user.bills;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 20/08/2016.
+ */
+public class Bills extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
